4e3deabf95ddbce16641e27fbcdfbbb642817c5d
Erik Pintar <epintar@andrew.cmu.edu>
2020-07-30 11:03:25 -0400

    inital commit - tried doing drag, changed mind to two clicks for zoom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PhotoInfo, Relic } from './types';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'relic-finder';
+
+  photoInfo: PhotoInfo = {
+    photoIdName: 'CDEF',
+    photoImgPath: 'assets/pics/CDEF.jpg',
+    relicsInPhoto: [0, 1],
+    photosInPhoto: ['D'],
+  };
+
+  relics: Relic[] = [
+    {
+      relicId: 0,
+      saint: {name: 'St. Francis of Assisi',
+      feastDayAndMonth: 'October 4',
+      vocation: 'founder'},
+      chapelLocation: 'D42',
+    },
+  ];
+
+
+
+  addZoomArea(coords: [number, number]): void {
+  }
+
+  addRelic(coords: [number, number]): void {
+    console.log(coords[0], coords[1]);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CabinetSceneComponent } from './cabinet-scene/cabinet-scene.component';
+import { ZoomAreaComponent } from './zoom-area/zoom-area.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabinetSceneComponent,
+    ZoomAreaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
