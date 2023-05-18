7312d22325d3793dd5f50c973527eeecd0550449
Erik Pintar <elpintar@gmail.com>
2023-05-18 16:13:29 -0400

    actually add new search-bar files; get rid of local assets/pics

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Saint } from '../types';
+import { map, startWith } from 'rxjs/operators';
+import { FirebaseDataService } from '../firebase-data.service';
+import { makeSaintNameString } from '../helperFuncs';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.sass']
+})
+export class SearchBarComponent {
+  autocompleteSaintsCtrl = new FormControl();
+  filteredSaints: Observable<Saint[]>;
+  getHumanReadableSaintName: Function;
+
+  constructor(
+    private firebaseDataService: FirebaseDataService) {
+    this.getHumanReadableSaintName = makeSaintNameString;
+    this.filteredSaints = this.autocompleteSaintsCtrl.valueChanges
+      .pipe(
+        startWith(''),
+        map(this.filterSaintsMatchingSearch, this)
+      );
+  }
+
+  filterSaintsMatchingSearch(searchValue: string): Saint[] {
+    const copyOfAllSaints = this.firebaseDataService.allSaintsLocal.slice();
+    if (searchValue) {
+      const searchLowercase = searchValue.toLowerCase();
+      return copyOfAllSaints.filter(s => {
+        let sName = this.getHumanReadableSaintName(s);
+        sName = sName.toLowerCase();
+        return sName.indexOf(searchLowercase) >= 0;
+      });
+    } else {
+      return copyOfAllSaints;
+    }
+  }
+
+  autocompleteOptionSelectedForSaint(selectedSaint: Saint): void {
+    const relicsWithSaint = this.firebaseDataService.getRelicsForSaint(selectedSaint);
+    console.log(selectedSaint.firebaseDocId, relicsWithSaint);
+  }
+}
